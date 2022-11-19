e542a955fb3eaa45199bb8cdc5ff9d74ffb92ee1
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-19 22:47:34 +0700

    delete useEffect

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {ScrollView, Text, View} from 'react-native';
 import {Button, Input} from '../../components';
 import {colors} from '../../utils';
