ebdae5395c2fea23d29bb78b8c9883b99a69bf22
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-19 14:57:36 +0700

    slicing register page and add illustrator svg

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,5 @@
 import welcomeAuth from './illustrations/welcome_auth.png';
+import IconBack from './icon/back.svg';
+import IllustrationRegister from './illustrations/register.svg';
 
-export {welcomeAuth};
+export {welcomeAuth, IconBack, IllustrationRegister};
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,34 +1,53 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {Input} from '../../components';
+import {colors} from '../../utils';
+import {IconBack, IllustrationRegister} from '../../assets';
 
 const Register = () => {
   return (
-    <View style={{padding: 20}}>
-      <View style={{width: 25, height: 25, backgroundColor: 'blue'}} />
-      <View
-        style={{
-          width: 106,
-          height: 115,
-          backgroundColor: 'purple',
-          marginTop: 8,
-        }}
+    <View style={styles.wrapper.page}>
+      <IconBack width={25} height={25} />
+      <IllustrationRegister
+        width={106}
+        height={115}
+        style={styles.illustration}
       />
-      <Text
-        style={{
-          fontSize: 14,
-          fontWeight: 'bold',
-          color: 'purple',
-          marginTop: 16,
-          maxWidth: 200,
-        }}>
+      <Text style={styles.text.desc}>
         Please submit your data for the registration process
       </Text>
+      <View style={styles.space(64)} />
       <Input placeholder="Full Name" />
+      <View style={styles.space(33)} />
       <Input placeholder="Email" />
+      <View style={styles.space(33)} />
       <Input placeholder="Password" />
     </View>
   );
+};
+
+const styles = {
+  wrapper: {
+    page: {padding: 20},
+  },
+  illustration: {
+    marginTop: 8,
+  },
+  text: {
+    desc: {
+      fontSize: 14,
+      fontWeight: 'bold',
+      color: colors.default,
+      marginTop: 16,
+      maxWidth: 200,
+    },
+  },
+
+  space: value => {
+    return {
+      height: value,
+    };
+  },
 };
 
 export default Register;
