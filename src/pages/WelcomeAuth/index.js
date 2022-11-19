24c338941548d6850d04f029e713f2c76ca5f5b5
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-19 09:41:28 +0700

    modify actionButton

--- src/pages/WelcomeAuth/index.js
+++ src/pages/WelcomeAuth/index.js
@@ -1,41 +1,41 @@
 import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 
-const WelcomeAuth = () => {
-  const ActionButton = ({desc, title}) => {
-    return (
-      <View style={{marginBottom: 43, maxWidth: 225}}>
+const ActionButton = ({desc, title}) => {
+  return (
+    <View style={{marginBottom: 43, maxWidth: 225}}>
+      <Text
+        style={{
+          fontSize: 10,
+          color: '#7e7e7e',
+          textAlign: 'center',
+          paddingHorizontal: '15%',
+          marginBottom: 6,
+        }}>
+        {desc}
+      </Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#a55eea',
+          borderRadius: 25,
+          paddingVertical: 13,
+        }}>
         <Text
           style={{
-            fontSize: 10,
-            color: '#7e7e7e',
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: 'white',
+            textTransform: 'uppercase',
             textAlign: 'center',
-            paddingHorizontal: '15%',
-            marginBottom: 6,
           }}>
-          {desc}
+          {title}
         </Text>
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#a55eea',
-            borderRadius: 25,
-            paddingVertical: 13,
-          }}>
-          <Text
-            style={{
-              fontSize: 12,
-              fontWeight: 'bold',
-              color: 'white',
-              textTransform: 'uppercase',
-              textAlign: 'center',
-            }}>
-            {title}
-          </Text>
-        </TouchableOpacity>
-      </View>
-    );
-  };
+      </TouchableOpacity>
+    </View>
+  );
+};
 
+const WelcomeAuth = () => {
   return (
     <View
       style={{
